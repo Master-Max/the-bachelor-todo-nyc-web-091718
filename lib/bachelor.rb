0f135f45618ed4b19a98a3de95287a5466c5fddb
Max Petersen <mglxp5@gmail.com>
2018-09-13 20:42:20 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,7 @@ end
 def get_average_age_for_season(data, season)
   array_of_ages = []
   puts "====++++===="
-  data[season].collect do |x,y|
+  array_of_ages = data[season].collect do |x,y|
     y.collect do |w,z|
       w["age"].collect
     end
