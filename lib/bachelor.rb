3edabd958e678cb881c5cc47e4aa8e1b6d211517
Max Petersen <mglxp5@gmail.com>
2018-09-13 20:35:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,6 +41,7 @@ def count_contestants_by_hometown(data, hometown)
   puts "=====*****=====\n#{hash}"
   puts "=====-----====="
   puts hash.length
+  hash.length
 end
 
 def get_occupation(data, hometown)
